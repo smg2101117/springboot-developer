9481d7e1ed56e7408d9fd12fca9d47954cf2bf15
smg <tjalsry02@g.yju.ac.kr>
2026-03-05 12:25:07 +0900

    init: Spring Boot project - setup, controller test

---
+++ src/main/java/me/smg/springdeveloper/HelloWorldController.java
@@ -0,0 +1,28 @@
+package me.smg.springdeveloper;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    // http://localhost:8080/student?firstName=SungChul&lastName=Park
+    @GetMapping("/student")
+    public Student getStudent(@RequestParam("firstName") String firstName,
+                              @RequestParam("lastName") String lastName) {
+        return new Student(firstName, lastName);
+    }
+
+    // http://localhost:8080/student/ChanHo/Park
+    @GetMapping("/student/{firstName}/{lastName}")
+    public Student getStudent2(@PathVariable("firstName") String firstName,
+                               @PathVariable("lastName") String lastName) {
+        return new Student(firstName, lastName);
+    }
+}
---
+++ src/main/java/me/smg/springdeveloper/Student.java
@@ -0,0 +1,16 @@
+package me.smg.springdeveloper;
+
+public class Student {
+    private String firstname;
+    private String lastname;
+    public Student(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+}
