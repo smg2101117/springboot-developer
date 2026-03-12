98dc4ab94a9e14617b9f36eeac2a899a0cdb2ae2
smg <tjalsry02@g.yju.ac.kr>
2026-03-12 12:20:44 +0900

    2026-03-12 12:20

--- src/main/java/me/smg/springdeveloper/TestController.java
+++ src/main/java/me/smg/springdeveloper/TestController.java
@@ -3,17 +3,15 @@ package me.smg.springdeveloper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
-public class testController {
-
+public class TestController {
     @Autowired
-    private TestService testService;
-
+    TestService testService;
     @GetMapping("/test")
     public List<Member> getAllMembers() {
-        return testService.getAllMembers();
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
