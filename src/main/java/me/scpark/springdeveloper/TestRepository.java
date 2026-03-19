4d001f64f72646efbcaee687a7484d8e867e65dd
smg <tjalsry02@g.yju.ac.kr>
2026-03-19 12:35:38 +0900

    2026-03-19 12:35

--- src/main/java/me/scpark/springdeveloper/TestRepository.java
+++ src/main/java/me/scpark/springdeveloper/TestRepository.java
@@ -3,8 +3,7 @@ package me.scpark.springdeveloper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface TestRespository {
-    @Repository
-    public interface TestRepository extends JpaRepository<Member, Long> {
-    }
+@Repository
+public interface TestRepository extends JpaRepository<Member, Long> {
 }
+
